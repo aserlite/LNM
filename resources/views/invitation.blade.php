10d119565e1a8b7d07b3dde90beb538f2244e4f3
aserlite <arthurcuvillon691@gmail.com>
2022-11-19 10:22:32 +0100

    avancée qr code

--- resources/views/invitation.blade.php
+++ resources/views/invitation.blade.php
@@ -7,9 +7,9 @@
 @section('content')
 <div class="bvnhead">
   <span>On se revoit le 5 Janvier {{$prenom}}</span>
+  {{$qrcode}}
 </div>
-{{$qrcode}}
-<span class="warningspan">Merci de ne pas essayer de lire le qr code (ça serait dommage de ne pas pouvoir rentrer)</span>
+
 <a href='{{$url}}'>test</a>
-<a href='/' class="dlinvit">Télecharger mon billet</a>
+<a href='/test' class="dlinvit">Télecharger mon billet</a>
 @endsection
